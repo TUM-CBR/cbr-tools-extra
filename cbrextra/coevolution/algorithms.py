ba9bb3f7b586845848c736f595112fbc3c3c040b
Ernesto Rodriguez <ernesto.rodriguez@tum.de>
2024-04-18 09:09:40 +0000

    [CoEvolution] Function to score results and function to rank and query.

--- cbrextra/coevolution/algorithms.py
+++ cbrextra/coevolution/algorithms.py
@@ -1,7 +1,9 @@
 from Bio.Align import MultipleSeqAlignment
 from Bio.Data.IUPACData import protein_letters
 import pandas as pd
-from typing import List, NamedTuple, Optional
+from typing import cast, Iterable, List, NamedTuple, Optional
+
+from .data import *
 
 RESIDUE_TO_INT = {
     res: i
@@ -19,8 +21,8 @@ K_RESIDUE_1 = K_RESIDUE + LSUFFIX
 K_RESIDUE_2 = K_RESIDUE + RSUFFIX
 K_OCCURRENCE_COUNT = 'occurence count'
 K_OCCURENCE_SINGLE_COUNT = 'occurence single count'
-K_OCCURRENCE_SCORE = 'occurrence'
-K_EXLUSIVITY_SCORE = 'exclusivity'
+K_HETEROGENEITY_SCORE = "occurence heterogeneity"
+K_SCORE_ALL = "score"
 
 class CoEvolutionAnalysis(NamedTuple):
     """
@@ -75,25 +77,84 @@ class CoEvolutionAnalysis(NamedTuple):
             data = msa_df[msa_df[K_POSITION_1] != msa_df[K_POSITION_2]]
         )
     
+    @classmethod
+    def score_conserved(cls, result: pd.DataFrame) -> pd.Series[float]:
+        """
+        This scoring amis to exclude positions where residues are simply
+        always conserved. These are potenitally important relations but
+        not for the purpose of co-evolution. The idea is that the closer
+        the residue pair is to being fully conserved accross the whole
+        set of sequences, the more the pair gets penalized.
+
+        Parameters
+        ----------
+        result: pd.DataFrame
+            A DataFrame containing the K_OCCURRENCE_SCORE column with the occurence
+            or residue pairs relative to the total.
+        """
+
+        series =  result[K_OCCURRENCE_SCORE]
+        series = (1 - series)
+        low_mask  = (series <= 0.5) * 1
+        series = (series * low_mask)  + (low_mask * 0.5)
+        return series * 2
+        
+    def score_occurence(self, result: pd.DataFrame) ->  pd.Series[float]:
+        """
+        This function scores the occurence of a specific pair of residues relative
+        to the total number of sequences in the alignment.
+
+        
+        Parameters
+        ----------
+        series: pd.DataFrame
+            A DataFrame containing the counts per residue pair as the column named
+            K_OCCURRENCE_COUNT.
+        """
+
+        return result[K_OCCURRENCE_COUNT] / len(self.alignment)
+    
+    @classmethod
+    def score_exclusivity(cls, result: pd.DataFrame) -> pd.Series[float]:
+        """
+        This scoring aims to capture how often does this specifi residue
+        pairs occurs relative to the first residue of the pair occuring
+        paired with other residues. The intuition is that if two residues
+        are co-evolving, then one should expect to observe the second residue
+        whenever the first residue occurs. A score of 1 means that every
+        time the first residue of the pair occurs at the specified position,
+        then the second residue of the pair will follow.
+
+        Parameters
+        ----------
+        result: pd.DataFrame
+            A pandas DataFrame containing the colum K_OCCURRENCE_COUNT with
+            the counts of occurence of the reside pair and a column called
+            K_OCCURENCE_SINGLE_COUNT containing the counts of the occurence
+            of the first residue of the pair paired with any other residue.
+        """
+        return result[K_OCCURRENCE_COUNT] / result[K_OCCURENCE_SINGLE_COUNT]
+
     def score_positions(
         self,
         positions: List[int],
-        center: float
-    ):
+        scoring: Scoring
+    ) -> pd.DataFrame:
         """
         Query the co-evolution analysis using the correlated occurence of the
         residues at the positions provided with residues in any other positions
-        of the sequence alignment
+        of the sequence alignment.
         """
 
-        assert len(positions) > 0, "Position has to be provided to query."
+        if len(positions) < 1:
+            raise ValueError(f"{positions}: The length must be at least 1.")
 
         position = positions[0]
         data = self.data
         mask = data[K_POSITION_1] == position
 
         for position in positions[1:]:
-            mask |= data[K_POSITION_1]  == position
+            mask |= data[K_POSITION_1] == position
 
         data = data[mask]
         pair_counts = data.groupby(by=[K_POSITION_1, K_POSITION_2, K_RESIDUE_1, K_RESIDUE_2]).count()
@@ -125,6 +186,65 @@ class CoEvolutionAnalysis(NamedTuple):
             how='left'
         )
 
-        result[K_OCCURRENCE_SCORE] = result[K_OCCURRENCE_COUNT] / len(self.alignment)
-        result[K_EXLUSIVITY_SCORE] = result[K_OCCURRENCE_SCORE] / result[K_OCCURENCE_SINGLE_COUNT]
+        result[K_OCCURRENCE_SCORE] = score_occurence = self.score_occurence(result)
+        result[K_EXLUSIVITY_SCORE] = score_exclusivity = result[K_OCCURRENCE_SCORE] / result[K_OCCURENCE_SINGLE_COUNT]
+        result[K_CONSERVED_SCORE] = score_conserved = self.score_conserved(result)
+        result[K_SCORE_ALL] = (
+            scoring.occurence_weight * score_occurence
+            + scoring.exclusivity_weight * score_exclusivity
+            + scoring.conserved_weight * score_conserved
+        ) / 3
 
+        return result
+    
+    @classmethod
+    def __to_position_result(cls, ranked_position_scores: pd.DataFrame) -> CoevolutionPosition:
+        positions: pd.Series[int] = ranked_position_scores[K_POSITION_1] 
+        position = positions[0]
+
+        if not (positions == position).all():
+            raise ValueError()
+
+
+        positions_2: pd.Series[int] = ranked_position_scores[K_POSITION_2]
+        residues_1: pd.Series[int] = ranked_position_scores[K_RESIDUE_1]
+        residues_2: pd.Series[int] = ranked_position_scores[K_RESIDUE_2]
+        scores: pd.Series[float] = ranked_position_scores[K_SCORE_ALL]
+        score_occurence: pd.Series[float] = ranked_position_scores[K_OCCURRENCE_SCORE]
+        score_exclusivity: pd.Series[float] = ranked_position_scores[K_EXLUSIVITY_SCORE]
+        score_conserved: pd.Series[float] = ranked_position_scores[K_CONSERVED_SCORE]
+
+        return CoevolutionPosition(
+            position = position,
+            by_position = {
+                positions_2[i]: CoevolutionEntry(
+                    residue_1=protein_letters[residues_1[i]],
+                    residue_2=protein_letters[residues_2[i]],
+                    score=scores[i],
+                    score_occurence=score_occurence[i],
+                    score_exclusivity=score_exclusivity[i],
+                    score_conserved=score_conserved[i]
+                )
+                for i in cast(Iterable[int], ranked_position_scores.index)
+            }
+        )
+
+    def query_scores(
+        self,
+        positions: List[int],
+        results_per_position: int,
+        scoring: Scoring
+    ) -> CoevolutionResults:
+        scores = self.score_positions(positions, scoring)
+
+        coevolution_positions = {
+            position : self.__to_position_result(score)
+            for position in positions
+            for score in [scores[scores[K_POSITION_1] == position]]
+            for ranks in [score[K_SCORE_ALL].rank(ascending=False)]
+            for score in [score[ranks <= results_per_position]]
+        }
+
+        return CoevolutionResults(
+            positions = coevolution_positions
+        )
--- cbrextra/coevolution/data.py
+++ cbrextra/coevolution/data.py
@@ -1,20 +1,38 @@
 from pydantic import BaseModel
-from typing import Any, Dict, List, Literal, Optional, Union
+from typing import Any, Dict, List, Optional
 
-Scoring = Union[Literal['occurence'], Literal['symmetry']]
+K_OCCURRENCE_SCORE = 'occurrence'
+K_EXLUSIVITY_SCORE = 'exclusivity'
+K_CONSERVED_SCORE = 'conserved'
+
+class Scoring(BaseModel):
+    """
+    Wether to residues are co-evolving or not is decided based
+    on a score which constitutes many criteria. It is possible
+    to weight each criteria differently by changing the paramters
+    provided here.
+    """
+
+    occurence_weight: float = 1
+    exclusivity_weight: float = 1
+    conserved_weight: float = 1
 
 class Query(BaseModel):
     positions: List[int]
     max_results: int
-    scoring: Scoring
     scoring_args: Dict[Any, Any]
+    scoring: Scoring = Scoring()
 
 class InteractiveInput(BaseModel):
     query: Optional[Query]
 
 class CoevolutionEntry(BaseModel):
-    residue: str
+    residue_1: str
+    residue_2: str
     score: float
+    score_occurence: float
+    score_exclusivity: float
+    score_conserved: float
 
 class CoevolutionPosition(BaseModel):
     position: int
